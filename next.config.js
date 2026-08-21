f13441835f7c4b2d9c2c34cb2e2aa876e61bef88
milanbhattraee <stylermilan@gmail.com>
2026-08-21 13:46:49 +0545

    updated yml

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export', 
-  distDir: 'build',
-  images: {
-    domains: ['scholib.com'],
-    unoptimized: true,
-  },
 };
 
 module.exports = nextConfig;
